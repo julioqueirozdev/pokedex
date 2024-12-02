7d7481b3e421cd431f398fa635ed68498c00e77a
Júlio Queiroz <jcqaraujo@gmail.com>
2024-12-02 17:04:24 -0400

    feat: manipulando o response do fetch

--- src/script/script.js
+++ src/script/script.js
@@ -2,7 +2,9 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-    console.log(response)
-})
+fetch(url)
+.then((response) => response.json())
+.then((jsonBody) => console.log(jsonBody))
+.catch((error) => console.log(error))
 
+// arrow function => são sintaxes reduzidas de uma função nominal
