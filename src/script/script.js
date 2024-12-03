47714e3bde8f5342e8fe1e4afa9d00cc0d2d76af
Júlio Queiroz <jcqaraujo@gmail.com>
2024-12-03 17:16:30 -0400

    doc: separação do consumo de api do html

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,3 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -24,19 +20,17 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody)=> jsonBody.results)
-.then((pokemons) => {
+pokeApi.getPokemons().then((pokemons) => {
+    const listItens = []
     
     for (let i = 0; i < pokemons.length; i++) {
         const pokemon = pokemons[i];
-        console.log(convertPokemonToLi(pokemon))
-        pokemonList.innerHTML +=  convertPokemonToLi(pokemon);
+        listItens.push(convertPokemonToLi(pokemon))
     }
 
+    console.log(listItens)
+
 })
-.catch((error) => console.log(error))
+
 
 // arrow function => são sintaxes reduzidas de uma função nominal
